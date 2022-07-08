21629a9275e181e191d59b981c1a79aef3e7b929
Никита Бабеев <babeev.nikita@gmail.com>
2022-07-08 21:30:36 +0700

    switched base api url to https

--- src/app/api.ts
+++ src/app/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://5.130.78.41:27020/',
+    baseUrl: 'https://5.130.78.41:27020/',
   }),
   endpoints: () => ({}),
 })
